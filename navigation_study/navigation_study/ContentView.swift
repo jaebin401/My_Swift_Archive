f6137d78f2db574c4e41b71b5cf8d6c4ed10b973
Jaebin Ahn <anjaebin200701@gmail.com>
2026-05-20 16:09:29 +0900

    navigation basic

---
+++ navigation_study/navigation_study/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  navigation_study
+//
+//  Created by Jaebin Ahn on 5/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                NavigationLink("page 1") {
+                    Text("this is page 1")
+                }
+                .padding(16)
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(8)
+                
+                NavigationLink("page 2") {
+                    Text("this is page 2")
+                }
+                .padding(16)
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(8)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ navigation_study/navigation_study/navigation_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigation_studyApp.swift
+//  navigation_study
+//
+//  Created by Jaebin Ahn on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct navigation_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
