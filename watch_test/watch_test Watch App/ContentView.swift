fc85e8370a36a36a983a02a812171c38e1feb695
Jaebin Ahn <anjaebin200701@gmail.com>
2026-05-21 10:38:03 +0900

    Navigation으로 센서별 데이터 측정 기능 구현

--- watch_test/watch_test Watch App/ContentView.swift
+++ watch_test/watch_test Watch App/ContentView.swift
@@ -8,46 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    
+
     @StateObject private var motion = MotionManager()
     
-    var text1: String = "문장1"
-    var text2: String = "문장2"
-    
     var body: some View {
-        VStack(spacing: 30) {
+        
+        NavigationStack {
+            ScrollView{
+                VStack {
+                    Text("CoreMotion Test")
+                        .font(.title3)
+                        .bold()
                     
-            Text("Motion Test")
-                //.font(.title)
-                //.bold()
-            
-            // 숫자 값 표시
-            VStack(alignment: .leading, spacing: 10) {
-//                Text(String(format: "R: %+2.2f°", motion.roll))
-//                Text(String(format: "P: %+2.2f°", motion.pitch))
-//                Text(String(format: "Y: %+2.2f°", motion.yaw))
-                
-                Text(String(format: "R: %+2.2f°", motion.roll))
-                Text(String(format: "P: %+2.2f°", motion.pitch))
-                Text(String(format: "Y: %+2.2f°", motion.yaw))
-                
+                    NavigationLink("Magnetic"){
+                        Text("Magnetic")
+                            .bold()
+                        VStack(alignment: .leading, spacing: 10)
+                        {
+                            Text(String(format: "R: %+2.2f°", motion.magRoll))
+                            Text(String(format: "P: %+2.2f°", motion.magPitch))
+                            Text(String(format: "Y: %+2.2f°", motion.magYaw))
+                        }.onAppear {
+                            motion.magnetic()
+                        }
+                        .onDisappear {
+                            motion.stop()
+                        }
+                        .font(.system(.title3, design: .monospaced))
+                        .padding()
+                        .background(Color.gray.opacity(0.15))
+                        .cornerRadius(12)
+                    }
+                    
+                    NavigationLink("Accelerometer"){
+                        Text("Accelerometer")
+                        VStack(alignment: .leading, spacing: 10)
+                        {
+                            Text(String(format: "R: %+2.2f°", motion.roll))
+                            Text(String(format: "P: %+2.2f°", motion.pitch))
+                            Text(String(format: "Y: %+2.2f°", motion.yaw))
+                        }.onAppear {
+                            motion.motionStart()
+                        }
+                        .onDisappear {
+                            motion.stop()
+                        }
+                        .font(.system(.title3, design: .monospaced))
+                        .padding()
+                        .background(Color.gray.opacity(0.15))
+                        .cornerRadius(12)
+                    }
+                    
+                    NavigationLink("Accel"){
+                        Text("Accelerometer")
+                            .bold()
+                        VStack(alignment: .leading, spacing: 10)
+                        {
+                            Text(String(format: "X: %+2.2f°", motion.accelX))
+                            Text(String(format: "Y: %+2.2f°", motion.accelY))
+                            Text(String(format: "Z: %+2.2f°", motion.accelZ))
+                        }.onAppear {
+                            motion.acceleration()
+                        }
+                        .onDisappear {
+                            motion.stop()
+                        }
+                        .font(.system(.title3, design: .monospaced))
+                        .padding()
+                        .background(Color.gray.opacity(0.15))
+                        .cornerRadius(12)
+                    }
+                }
             }
-            .font(.system(.title3, design: .monospaced))
-            .padding()
-            .background(Color.gray.opacity(0.15))
-            .cornerRadius(12)
+            //.padding()
+            // View가 화면에 나타날 때 센서 시작
             
-            
-        }
-        .padding()
-        // View가 화면에 나타날 때 센서 시작
-        .onAppear {
-            motion.start()
-        }
-        // View가 사라질 때 센서 정지
-        .onDisappear {
-            motion.stop()
         }
     }
 }
