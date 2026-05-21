c5f8660d0cab22e67a330562d23173d76694c8cd
Jaebin Ahn <anjaebin200701@gmail.com>
2026-05-21 10:38:35 +0900

    가속도계, imu, 자기센서 계측내용 메서드 MotionManager추가

--- watch_test/watch_test Watch App/MotionManager.swift
+++ watch_test/watch_test Watch App/MotionManager.swift
@@ -14,8 +14,12 @@ class MotionManager: ObservableObject {
     @Published var magPitch: Double = 0.0
     @Published var magYaw: Double = 0.0
     
+    @Published var accelX: Double = 0.0
+    @Published var accelY: Double = 0.0
+    @Published var accelZ: Double = 0.0
+    
     // 센서 업데이트 시작
-    func start() {
+    func motionStart() {
         
         guard motionManager.isDeviceMotionAvailable else {
             print("⚠️ Device motion not available. 실제 기기에서 실행하세요.")
@@ -28,19 +32,54 @@ class MotionManager: ObservableObject {
             guard let self = self, let motion = motion else { return }
 
             let attitude = motion.attitude
-            let magnet = motion.magneticField
             
             self.roll  = attitude.roll  * 180.0 / .pi
             self.pitch = attitude.pitch * 180.0 / .pi
             self.yaw   = attitude.yaw   * 180.0 / .pi
+        }
+
+    }
+    
+    func magnetic() {
+        
+        guard motionManager.isMagnetometerAvailable else { return }
+        
+        motionManager.magnetometerUpdateInterval = 1.0 / 60.0
+        motionManager.startMagnetometerUpdates(to: .main) { [weak self] data, error in
+            guard let self, let data else { return }
             
-            self.magRoll = magnet.field.x
-            self.magPitch = magnet.field.y
-            self.magYaw = magnet.field.z
+            let f = data.magneticField       // CMMagneticField (μT 단위)
+            self.magRoll  = f.x
+            self.magPitch = f.y
+            self.magYaw   = f.z
+        }
+    }
+    
+    func acceleration() {
+        
+        guard motionManager.isAccelerometerAvailable else {
+               print("⚠️ Accelerometer not available")
+               return
+           }
+        
+        motionManager.accelerometerUpdateInterval = 1.0 / 60.0
+        motionManager.startAccelerometerUpdates(to: .main) { [weak self] acceleration, error in
+            
+            guard let self = self, let accel = acceleration else { return }
+            
+            let x = accel.acceleration.x
+            let y = accel.acceleration.y
+            let z = accel.acceleration.z
+            
+            self.accelX = x
+            self.accelY = y
+            self.accelZ = z
         }
     }
     
     func stop() {
+        motionManager.stopDeviceMotionUpdates()
+        motionManager.stopMagnetometerUpdates()
         motionManager.stopDeviceMotionUpdates()
     }
 }
