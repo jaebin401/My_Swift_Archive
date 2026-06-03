5b817fced9dc76f5de1d4ebfc25a29ef38ebe26b
Jaebin Ahn <anjaebin200701@gmail.com>
2026-06-03 13:00:31 +0900

    주석추가

--- navigation_study/navigation_study/ButtonComponent.swift
+++ navigation_study/navigation_study/ButtonComponent.swift
@@ -5,6 +5,14 @@
 //  Created by Jaebin Ahn on 5/29/26.
 //
 
+// 이 코드는 단순히 NavigationLink 버튼 자체만을 담고있는 컴포넌트이다.
+// 그러면 이 버튼을 클릭했을 때 이동하는 destination NavigationLink는 어디냐?
+// -> 실제 이 컴포넌트가 사용될 부분에서 중괄호로 지정될 뷰에서 중괄호에 들어갈 부분이다
+//      e.g
+//      ButtonComponent(title: "버튼 이름") {
+//           subPage() // 여기로 이동함
+//      }
+
 import SwiftUI
 
 struct ButtonComponent<Destination: View>: View {
